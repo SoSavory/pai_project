a7cdeefd38f089204e624fa605a7f506ed0fc5e6
Avery Marsh <avery@alumni.unc.edu>
2024-09-22 00:45:12 -0400

    added a path history function and gui. refactored gui to be left/right. added helper classes.

--- main.js
+++ main.js
@@ -94,12 +94,20 @@ class Wizard{
         this.graph = new WizardGraph(decisionTreePrototype, [], "start");
     }
     listEdges(node_id){
-        // I just want to be clear that this should never modify an edge
-        const rawEdges = JSON.parse(JSON.stringify(this.graph.decision_tree[node_id].edges));
-        // set where each edge originated from when listing them. idk this just seemed like the most convenient place to put this,
-        // but it would probably be fine to just run this on the graph at the start
-        const honedEdges = rawEdges.map((re) => ({...re, from: String(node_id)}));
-        return honedEdges;
+        const node = this.graph.decision_tree[node_id];
+        if(node.edges){
+            // I just want to be clear that calling listEdges should never return an object that could modify an edge
+            const rawEdges = JSON.parse(JSON.stringify(this.graph.decision_tree[node_id].edges));
+            // set where each edge originated from & its associated prompt when listing them. idk this just seemed like the most convenient place to put this,
+            // but it would probably be fine to just run this on the graph at the start
+            const honedEdges = rawEdges.map((re) => ({...re, from: String(node_id), prompt: node.prompt}));
+            return honedEdges.map((he) => (new GraphEdge(he)));
+        } else{
+            return [];
+        }
+    }
+    listPath(){
+        return JSON.parse(JSON.stringify(this.graph.path));
     }
     // argument edge is a plain object shaped as such: {from: node_id, to: node_id, label: ""}
     // this is the event which signals an update to state/position, as we move from our current position along the specified edge, 
@@ -107,51 +115,40 @@ class Wizard{
     traverseEdge(edge){
         // the object which represents the destination node, edge.to
         const to_node = this.graph.decision_tree[edge.to];
-
-        // 
-
+        // the object which will store which gui elements we want to create and attach
+        var left_contents = [];
+        var right_contents = [];
         // check if we have arrived at an end state, and if so, trigger it.
         if(to_node.is_pai){
-            this.isPAI();
-            return "pai";
-        } else if(to_node.is_not_pai){
-            this.isNotPAI();
-            return "not pai";
+            left_contents.push(this.domStubIsPAI());
+        } else if(to_node.is_not_pai){      
+            left_contents.push(this.domStubIsNotPAI());
+        } else{
+            left_contents.push(this.domStubActivities(to_node.prompt, this.listEdges(edge.to)));
         }
 
-        // destination node's prompt, a string
-        const newPrompt = to_node.prompt; 
-        // destination node's answers, an array of objects just like main argument edge
-        const newAnswers = this.listEdges(edge.to);
+        right_contents.push(this.domStubStartOverButton());
 
+        // update our path
+        this.graph.path.push(edge);
+        right_contents.push(this.domStubPath(this.listPath())); 
+
+        const w = this.domStubWizard(left_contents, right_contents);
         // rebuild the html/gui to reflect the new prompt and answers for our destination node
-        buildContents(
-            [
-                this.domStubActivities(newPrompt, newAnswers),
-                {"path-container": {}},
-                this.domStubStartOverButton(),
-            ],
-            document.getElementById(this.root_dom_id));
+        buildContents(w, document.body);
         return "traverse edge";
-    }
-    // end state method
-    isPAI(){
-        buildContents([
-            {"end-state": {type: "h3", textStart: "The Activity as Described is PAI compliant"}},
-        ], document.getElementById("activity-container"));
-    }
-    // end state method
-    isNotPAI(){
-        buildContents([
-            {"end-state": {type: "h3", textStart: "The Activity as Described is NOT PAI compliant"}},
-        ], document.getElementById("activity-container"));
     }
     // =================================================================================================
     // click event helper methods- 
     // accesses itself from the outside in (document.wizard = this), so click event may be transported and attached from outside of this object
     // =================================================================================================
     clickTraverseEdge(e){
-        document.wizard.traverseEdge({to: e.target.dataset.to_node, label: e.target.dataset.label});
+        document.wizard.traverseEdge(new GraphEdge({
+            to: e.target.dataset.edge_to_node,
+            label: e.target.dataset.edge_label,
+            from: e.target.dataset.edge_from_node,
+            prompt: e.target.dataset.edge_prev_node_prompt
+        }));
     }
     clickStartOver(e){
         document.wizard.start();
@@ -160,64 +157,106 @@ class Wizard{
     // domStub helper methods- 
     // they return the domStubs (passed to buildContents), which contain the data necessary to build/update fragments of our html.
     // =================================================================================================
+    // end state method
+    domStubIsPAI(){
+        return new domStubTree({html_id: "end-state", type: "h3", textStart: "The Activity as Described is PAI compliant"});
+    }
+    // end state method
+    domStubIsNotPAI(){
+        return new domStubTree({html_id: "end-state", type: "h3", textStart: "The Activity as Described is NOT PAI compliant"});
+    }
     domStubStartOverButton(){
-        return new Object( 
-            {"start-over-button": {type: "button", textStart: "Start Over", onClick: document.wizard.clickStartOver}}
-        );
+        return new domStubTree({html_id: "start-over-button", type: "button", textStart: "Start Over", onClick: document.wizard.clickStartOver});
     }
     domStubPrompt(promptText){
-        return new Object({"prompt-container": {textStart: "Prompt: " + promptText}});
+        return new domStubTree({html_id: "prompt-container", textStart: "Prompt: " + promptText});
     }
-    // argument edges is an array of {from: node_id, to: node_id, label: "text"} shaped objects.
+    // argument edges is an array of GraphEdges.
     domStubAnswers(edges){
         var edges_html = new Array(edges.length);
         // root node for answers
-        const answers_html_base = {"answer-container": {type: "ul", children: edges_html}};
+        const answers_html_base = new domStubTree({html_id: "answer-container", type: "ul", children: edges_html});
         // loop through edges and generate html nodes.
         // each iteration depletes the main argument edges, think of it as a queue. 
         while(edges.length > 0){
+            
             const a = edges.pop();
-            var edge_html = {};
             // use the length of the depleting edges array to create unique ids for our dom stub child nodes
-            const edge_html_id = "edge-container-" + String(a.to) + "_" + String(edges.length);
-
-            edge_html[edge_html_id] = {
+            const edge_html = {
+                html_id: "edge-container-" + String(a.to) + "_" + String(edges.length),
                 type: "li", 
                 textStart: a.label,
-                dataList: [{key: "from_node", value: a.from}, {key: "to_node", value: a.to}, {key: "label", value: a.label}],
+                dataList: [
+                    {key: "edge_from_node", value: a.from}, 
+                    {key: "edge_to_node", value: a.to}, 
+                    {key: "edge_label", value: a.label},
+                    {key: "edge_prev_node_prompt", value: a.prompt}
+                ],
                 classList: ["clickable-edge"],
                 onClick: document.wizard.clickTraverseEdge,
             };
-            answers_html_base["answer-container"].children[edges.length] = edge_html;
+            edges_html[edges.length] = edge_html;
+            
         }        
         return answers_html_base;
     }
     domStubActivities(promptData, answerData){
         const promptContents = this.domStubPrompt(promptData);
         const answerContents = this.domStubAnswers(answerData);
-        
-        return new Object(
-            {"activity-container": {children: [answerContents, promptContents]}}
-        );
+        return new domStubTree({html_id: "activity-container", children: [promptContents, answerContents]});
+    }
+    domStubPath(path){
+        var steps_html = new Array(path.length+1);
+        steps_html[0] = new domStubTree({html_id: "paths-container-header-row", type: "tr", children: [
+            new domStubTree({type: "th", textStart: "Prompt"}), new domStubTree({type: "th", textStart: "Answer"})
+        ]});
+        // root node for answers
+        const steps_html_base = new domStubTree({html_id: "paths-container", type: "table", children: steps_html});
+        // loop through edges and generate html nodes.
+        // each iteration depletes the main argument edges, think of it as a queue. 
+        while(path.length > 0){
+            
+            const a = path.pop();
+            // use the length of the depleting edges array to create unique ids for our dom stub child nodes
+            const step_html = {
+                html_id: "step-container-" + String(a.to) + "-" + String(a.from) + "_" + String(path.length),
+                type: "tr", 
+                classList: ["path-table-step"],
+                children: [
+                    new domStubTree({type: "td", textStart: String(a.prompt)}),
+                    new domStubTree({type: "td", textStart: String(a.label)})
+                ]
+            };
+            steps_html[path.length+1] = step_html;
+        }      
+        return steps_html_base;
+    }
+    domStubWizard(left_contents, right_contents){
+        var wizard_html_base = [
+            new domStubTree({type: "header", textStart: "Welcome to the PAI Innovation Team Demo"}),
+            new domStubTree({html_id: "wizard-container", children: 
+                [
+                    new domStubTree({type: "div", html_id: "wizard-left", classList: ["left"], children: left_contents}),
+                    new domStubTree({type: "div", html_id: "wizard-right", classList: ["right"], children: right_contents})
+                ]
+            })
+        ];
+        return wizard_html_base;
     }
     // =============================================================================================
-    // initializer + starter
+    // starter
     // =============================================================================================
-    initialize(){
-        buildContents(
-            [
-                this.domStubActivities("", []),
-                {"path-container": {}},
-                this.domStubStartOverButton(),
-            ],
-            document.getElementById(this.root_dom_id)
-        );
-    }
     // attach itself (this) to the document, and "travel" to the starting node
     start(){
         document.wizard = this;
-        document.wizard.initialize();
-        document.wizard.traverseEdge({from: "", to: document.wizard.graph.root_node_id, label: ""});
+        this.graph.path = [];
+        const nullEdge = new GraphEdge({
+            from: document.wizard.graph.root_node_id, 
+            to: document.wizard.graph.root_node_id,
+            label: " ", 
+            prompt: "Start"
+        });
+        document.wizard.traverseEdge(nullEdge);
     }
 }
 
@@ -230,7 +269,55 @@ class WizardGraph{
         this.decision_tree_string = JSON.stringify(this.decision_tree);
         this.root_node_id         = root_node_id;
         this.root_node            = this.decision_tree[this.root_node_id];
-        this.path                 = path || [ {to: this.root_node_id, label: ""} ];
+        this.path                 = path || [];
+        this.cached_path          = JSON.stringify(this.path);
+    }
+}
+
+class GraphEdge{
+    constructor(obj){
+        if(obj.to){
+            this.to = String(obj.to);
+        }
+        if(obj.from){
+            this.from = String(obj.from);
+        }
+        if(obj.prompt){
+            this.prompt = String(obj.prompt);
+        }
+        if(obj.label){
+            this.label = String(obj.label);
+        }
+    }
+}
+
+class domStubTree{
+    constructor(obj){
+        if(obj.html_id){
+            this.html_id = String(obj.html_id);
+        }
+        if(obj.type){
+            this.type = String(obj.type);
+        } else{
+            this.type = "div";
+        }
+        if(obj.textStart){
+            this.textStart = String(obj.textStart);
+        }
+        if(obj.textEnd){
+            this.textEnd = String(obj.textEnd);
+        }
+        if(obj.onClick){
+            this.onClick = obj.onClick;
+        }
+        if(obj.dataList){
+            this.dataList = obj.dataList;
+        }
+        if(obj.children){
+            this.children = obj.children;
+        } else{
+            this.children = false;
+        }
     }
 }
 
@@ -244,73 +331,60 @@ function clearContainer(containerReference){
     return containerReference;
 }
 
-// recursively builds out a gui from a dom stub represented by a plain object structured as a tree.
+// recursively builds out a gui from an array of domStubTree objects.
 // appends html into target dom reference
 function buildContents(tree, target, childProcess=false){
-    // tree looks like: [{html_id: {content: {type: , textNode: , children: [{key=html id: {}}] }}}]
-    var contents = [];
 
-    // clear target. oh you don't want to have to write out creating elements? boo hoo, make more methods and get better at writing trees. caching could happen here
+    // clear target. oh you don't want to have to write out creating elements? boo hoo, make more methods and get better at writing trees. 
+    // caching could happen here :)
     if(!childProcess){
         clearContainer(target);
     }
     
+    // depth first search through tree
     for(const leaf of tree){
-        const dom_id = Object.keys(leaf)[0];
-        const node = leaf[dom_id] || {};
-        const dom_node = getOrCreateElement(dom_id, node.type);
+       
+        const dom_node = document.createElement(leaf.type || "div");
 
-        // Add text if necessary
-        if(node.textStart){
-            dom_node.appendChild(document.createTextNode(node.textStart));
+        if(leaf.html_id){
+            dom_node.id = leaf.html_id;
         }
 
-        if(node.classList){
-            for(c of node.classList){
+        // Add text if necessary
+        if(leaf.textStart){
+            dom_node.appendChild(document.createTextNode(leaf.textStart));
+        }
+
+        if(leaf.classList){
+            for(c of leaf.classList){
                 dom_node.classList += c;
             }
         }
 
         // comes in as [{key: "key", value: "value"}...]
-        if(node.dataList){
-            for(d of node.dataList){
+        if(leaf.dataList){
+            for(d of leaf.dataList){
                 dom_node.dataset[d.key] = d.value;
             }
         }
 
-        if(node.onClick){
-            dom_node.addEventListener("click", node.onClick);
+        if(leaf.onClick){
+            dom_node.addEventListener("click", leaf.onClick);
         }
 
-        // Recurse on children.
-        // Important to clear before adding children.
-        if(node.children){
-            buildContents(node.children, dom_node, true);
+        // Recurse on children. Depth first.
+        // Important to clear ~before~ adding children.
+        if(leaf.children){
+            buildContents(leaf.children, dom_node, true);
         }
 
-        if(node.textEnd){
-            dom_node.appendChild(document.createTextNode(node.textEnd));
+        if(leaf.textEnd){
+            dom_node.appendChild(document.createTextNode(leaf.textEnd));
         }
-
-        contents.push(dom_node);
+        
+        target.appendChild(dom_node);
     }
-
-    while(contents.length > 0){
-      
-        const element = contents.pop();
-        target.appendChild(element);
-    }
-    
     return target;
-}
-
-function getOrCreateElement(id, type="div"){
-    var element = document.getElementById(id);
-    if(!element){
-        element = document.createElement(type);
-        element.id = id;
-    }
-    return element;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
